b042d9048532f29698a30f13cdc0338b88ce6e6f
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-08-04 17:46:23 -0300

    Refactore promises to async/await syntax and remove initial boilerplate created by create-react-app

--- services/passport.js
+++ services/passport.js
@@ -23,18 +23,17 @@ passport.use(
 			callbackURL: '/auth/google/callback',
 			proxy: true
 		},
-		(accessToken, refreshToken, profile, done) => {
-			User.findOne({ googleId: profile.id }).then(existingUser => {
-				if (existingUser) {
-					// It already has a User created to the db
-					done(null, existingUser);
-				} else {
-					// It creates a new User to the db
-					new User({ googleId: profile.id }).save().then(user => {
-						done(null, user);
-					});
-				}
-			});
+		async (accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({ googleId: profile.id });
+
+			if (existingUser) {
+				// It already has a User created to the db
+				done(null, existingUser);
+			} else {
+				// It creates a new User to the db
+				const user = await new User({ googleId: profile.id }).save();
+				done(null, user);
+			}
 		}
 	)
 );

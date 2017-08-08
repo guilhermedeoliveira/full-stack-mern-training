bb98c4d636b065957abe941a2fe44147459405e5
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-08-08 09:35:02 -0300

    Complete auth flow with Header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,13 +9,13 @@ class Header extends Component {
 			case false:
 				return (
 					<li>
-						<a href="/auth/google">Login With Google</a>
+						<a href="/auth/google">Login with Google</a>
 					</li>
 				);
 			default:
 				return (
 					<li>
-						<a href="">Logout</a>
+						<a href="/api/logout">Logout</a>
 					</li>
 				);
 		}

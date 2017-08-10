e30f9ef44b33d204e30c21f6719551bcbd133930
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-08-10 11:02:23 -0300

    Backend: create RequireLogin middleware, pass it into stripe route handler and save user to db with credits

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,13 +1,20 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-	app.post('/api/stripe', async (req, res) => {
+	app.post('/api/stripe', requireLogin, async (req, res) => {
+		// Charges the user
 		const charge = await stripe.charges.create({
 			amount: 500,
 			currency: 'usd',
 			description: 'Charge 5$ dollars for 5 credits',
 			source: req.body.id
 		});
+		// Updates the credits
+		req.user.credits += 5;
+		const user = await req.user.save();
+		// Send user to client
+		res.send(user);
 	});
 };

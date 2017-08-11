c222df1b11f3461a8e434e128f90af5d25c55be1
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-08-11 19:26:17 -0300

    Init post new survey route handler and wire up middlewares to it

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 
 const keys = require('./config/keys');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 // Connect to remote MongoDB instance
@@ -26,6 +27,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
 	// Express will serve up production assets like main.js and main.css files

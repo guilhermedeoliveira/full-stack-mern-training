9dc8cf9687218f6423a3d0924b92995732a34405
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-08-14 16:25:07 -0300

    Backend: finish email send feature

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,7 +7,11 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-	app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+	app.get('/api/surveys/thanks', (req, res) => {
+		res.send('Thank you for voting!');
+	});
+
+	app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
 		//es6 restructuring. Out of req.body, I want to get all of these properties
 		//off the body thats added to req. The actual adding logic has to be added in.
 		const { title, subject, body, recipients } = req.body;
@@ -28,6 +32,16 @@ module.exports = app => {
 
 		//place to send the email
 		const mailer = new Mailer(survey, surveyTemplate(survey));
-		mailer.send();
+
+		try {
+			await mailer.send();
+			await survey.save();
+			req.user.credits -= 1;
+			const user = await req.user.save();
+
+			res.send(user);
+		} catch (err) {
+			res.status(422).send(err);
+		}
 	});
 };

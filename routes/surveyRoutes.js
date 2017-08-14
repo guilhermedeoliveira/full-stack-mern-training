0ff5f974d9f0b239043a96d77d6a10c2203dee2e
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-08-14 14:32:25 -0300

    Backend: config Mailer

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose'); // This approach is better for testing
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys'); // This approach is better for testing
 
@@ -16,5 +18,9 @@ module.exports = app => {
 			_user: req.user.id,
 			dateSent: Date.now()
 		});
+
+		// Send email
+		const mailer = new Mailer(survey, surveyTemplate(survey));
+		mailer.send();
 	});
 };

1ae8d73f883a3576b35867813bf9cdb6b4a9a9bf
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-08-16 16:32:26 -0300

    Frontend: wire up action creator to submit survey

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -4,8 +4,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import formFields from './formFields';
+import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
 	const reviewFields = _.map(formFields, ({ name, label }) => {
 		return (
 			<div key={name}>
@@ -23,8 +24,20 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
 		<div>
 			<h5>Please, review your entries</h5>
 			{reviewFields}
-			<button className="yellow darken-3 btn-flat" onClick={onCancel}>
+			<button
+				className="yellow darken-3 btn-flat white-text"
+				onClick={onCancel}
+			>
 				Back
+				<i className="material-icons left">keyboard_arrow_left</i>
+			</button>
+			<button
+				onClick={() => submitSurvey(formValues)}
+				type="submit"
+				className="green btn-flat right white-text"
+			>
+				Submit Survey
+				<i className="material-icons right">done_all</i>
 			</button>
 		</div>
 	);
@@ -34,4 +47,4 @@ function mapStateToProps(state) {
 	return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(SurveyFormReview);
